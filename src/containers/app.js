581854b8ddd67cf4638f81cb588837c95e334cfe
David Schovanec <schovanec@schovi.cz>
2016-02-26 21:51:37 +0100

    Handle of adding new todo
    1) Add value of new todo into state
    2) Create handler `handleChange` for new todo change
      a) handler is just a instance function on component
      b) bind it to event `onChange={(event) => this.handleChange(event)}`
      c) in common js it is function(event) { this.handleChange(event)}
      d) but arrow function `() => {}` bind and keep current context
      e) in handler we setState with new value
         `this.setState({newTodo: event.target.value})`
    3) Create handler `handleKeyDown` for listening on key press
      a) creating and binding is same as for change
      b) in handler we clear `state.newTodo` and create new todo and push
         it to `state.todos`

--- src/containers/app.js
+++ src/containers/app.js
@@ -8,8 +8,40 @@ export default class App extends React.Component {
         {id: 1, text: "Welcome to React Lesson", completed: true},
         {id: 2, text: "Kick off repository with lesson"},
         {id: 3, text: "Run development server"}
-      ]
+      ],
+      newTodo: ""
     }
+  }
+
+  handleChange(event) {
+    this.setState({newTodo: event.target.value})
+  }
+
+  handleKeyDown(event) {
+    // Filter out non enter keys
+    if (event.keyCode !== 13) {
+      return
+    }
+
+    event.preventDefault()
+
+    const newTodo = this.state.newTodo.trim()
+
+    if (newTodo) {
+    	this.setState({ newTodo: '' })
+      this.addTodo(newTodo)
+    }
+  }
+
+  addTodo(text) {
+    const todos = this.state.todos
+
+    todos.push({
+      id:   todos.length + 1,
+      text: text
+    })
+
+    this.setState({ todos: todos })
   }
 
   renderTodo(todo) {
@@ -29,7 +61,12 @@ export default class App extends React.Component {
       <section className="todoapp">
         <header className="header">
           <h1>todos</h1>
-          <input className="new-todo" placeholder="What needs to be done?" autofocus />
+          <input value={this.state.newTodo}
+                 className="new-todo"
+                 placeholder="What needs to be done?"
+                 onChange={(event) => this.handleChange(event)}
+                 onKeyDown={(event) => this.handleKeyDown(event)}
+                 autofocus />
         </header>
         <section className="main">
           <input className="toggle-all" type="checkbox" />
